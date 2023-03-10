81749b9e7d42516137a6e2a8b19f5cb1926fa8b3
Jerry Vargas <jerry.vargas@tabs.toshiba.com>
2023-03-09 18:31:33 -0800

    added azure functions Storage and Deployed Static web page added CDN

--- frontend/main.js
+++ frontend/main.js
@@ -2,11 +2,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
 getVisitCount();
 
 })
-
-const functionApi = 'http://localhost:7071/api/HttpTrigger1';
+const functionApiUrl = "https://azureresumecount1.azurewebsites.net/api/HttpTrigger1?code=3qNhg1MVi3ZqmM7s1nPKKcJ8tiy6rTm4zI48GPaaDoQWAzFuCQDp-w==";
+const localfunctionApi = 'http://localhost:7071/api/HttpTrigger1';
 const getVisitCount = () => {
 let count= 30;
-fetch(functionApi).then(response => {
+fetch(functionApiUrl).then(response => {
 return response.json()
 
 
